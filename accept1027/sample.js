e1172d0c38a74e6c9d533f4b833c11e171816387
jumpei-miyata <jumpei-miyata@cybozu.co.jp>
2015-01-23 16:52:22 +0900

    first commit.

---
+++ accept1027/sample.js
@@ -0,0 +1,7 @@
+(function() {
+    var marker = document.createElement('div');
+    marker.textContent = 'This app is customized';
+    marker.className = 'klog953-js-customize-done';
+    kintone.app.getHeaderSpaceElement().appendChild(marker);
+})();
+
